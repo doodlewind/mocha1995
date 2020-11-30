6da0435b39cc1674613c230a39ada929b4cdc061
Yifeng Wang <i@ewind.us>
2020-12-01 06:00:18 +0800

    feat: support wasm and js build

--- README.md
+++ README.md
@@ -1,5 +1,37 @@
-# Mocha
-The original Mocha JavaScript engine from [Netscape Communicator 3.0.2 Source Tree](https://archive.org/details/netscape-communicator-3-0-2-source).
+# Mocha 1995
+The original Mocha JavaScript engine, ported from [Netscape 3.0.2 Source Tree](https://archive.org/details/netscape-communicator-3-0-2-source).
+
+## History
+"Mocha" was the code name of the first JavaScript engine, originally written by Brendan Eich in his 10-day May 1995 sprint. For all of 1995 and most of 1996, Eich was the the only Netscape developer working fulltime on the JavaScript engine. Untill August 1996, the Mocha codebase still consisted primarily of code from the 10-day prototype.
+
+In late 1996, Eich "stayed home for two weeks to rewrite Mocha as the codebase that became known as SpiderMonkey", the JavaScript engine later released as open-source and currently maintained by the Mozilla Foundation.
+
+Here is the source code of production version of Mocha, which ships as JavaScript 1.1. This version is released in August 1996 with Netscape 3.0. The release of JavaScript 1.1 marks the completion of the initial definition and development of JavaScript.
 
 ## Build
-TODO
+The ported Mocha engine can be compiled to WASM, JS and native binary. For WASM and JS build, please make sure [Emscripten](https://emscripten.org/docs/getting_started/downloads.html) is installed and activated (`emcc` command is available):
+
+``` sh
+$ source build.sh
+
+# build WASM
+$ compile_wasm
+
+# build js
+$ compile_js
+
+# build native
+$ compile_native
+```
+
+The Web build generates `out/mocha_shell_js.html` and `out/mocha_shell_wasm.html` as entry. For the native build, there is a binary runtime available:
+
+``` sh
+$ out/mo_shell tests/ack.mocha
+```
+
+## Anecdote
+The mocha1995 project was ported by Yifeng Wang, he is the translator of the Chinese edition of [JavaScript: The First 20 Years](https://cn.history.js.org/). This is the book co-authored by Allen Wirfs-Brock and Brendan Eich, which contains the link to Mocha's forgotten source code.
+
+## License
+⚠️ License status is unknown yet. Don't use in commerial projects!
--- build.sh
+++ build.sh
@@ -1,81 +1,100 @@
-CC=clang
-AR=ar
 
 # For testing ports only, unused for actual build process
 function test_headers() {
     echo "testing headers..."
-    CC include/prmacros.h -o out/prmacros.pch
-    CC include/prtypes.h -o out/prtypes.pch
-    CC include/mo_pubtd.h -o out/mo_pubtd.pch
-    CC include/mo_prvtd.h -o out/mo_prvtd.pch
-    CC include/mochaapi.h -o out/mochaapi.pch
-    CC include/mo_bcode.h -o out/mo_bcode.pch
-    CC include/mo_parse.h -o out/mo_parse.pch
-    CC include/mo_emit.h -o out/mo_emit.pch
-    CC include/mo_scan.h -o out/mo_scan.pch
-    CC include/prhash.h -o out/prhash.pch
-    CC include/mo_atom.h -o out/mo_atom.pch
-    CC include/mo_scope.h -o out/mo_scope.pch
-    CC include/mochalib.h -o out/mochalib.pch
-    CC include/mocha.h -o out/mocha.pch
-    CC include/prarena.h -o out/prarena.pch
-    CC include/prclist.h -o out/prclist.pch
-    CC include/mo_cntxt.h -o out/mo_cntxt.pch
-    CC src/alloca.h -o out/alloca.pch
-    CC include/prprf.h -o out/prprf.pch
-    CC include/prglobal.h -o out/prglobal.pch
-    CC include/prlog.h -o out/prlog.pch
-    CC include/prmem.h -o out/prmem.pch
-    CC include/prmjtime.h -o out/prmjtime.pch
-    CC include/prtime.h -o out/prtime.pch
-    CC include/prlong.h -o out/prlong.pch
-    CC include/prosdep.h -o out/prosdep.pch
-    CC include/prunixos.h -o out/prunixos.pch
-    CC include/prsync.h -o out/prsync.pch
+    $CC include/prmacros.h -o out/prmacros.pch
+    $CC include/prtypes.h -o out/prtypes.pch
+    $CC include/mo_pubtd.h -o out/mo_pubtd.pch
+    $CC include/mo_prvtd.h -o out/mo_prvtd.pch
+    $CC include/mochaapi.h -o out/mochaapi.pch
+    $CC include/mo_bcode.h -o out/mo_bcode.pch
+    $CC include/mo_parse.h -o out/mo_parse.pch
+    $CC include/mo_emit.h -o out/mo_emit.pch
+    $CC include/mo_scan.h -o out/mo_scan.pch
+    $CC include/prhash.h -o out/prhash.pch
+    $CC include/mo_atom.h -o out/mo_atom.pch
+    $CC include/mo_scope.h -o out/mo_scope.pch
+    $CC include/mochalib.h -o out/mochalib.pch
+    $CC include/mocha.h -o out/mocha.pch
+    $CC include/prarena.h -o out/prarena.pch
+    $CC include/prclist.h -o out/prclist.pch
+    $CC include/mo_cntxt.h -o out/mo_cntxt.pch
+    $CC src/alloca.h -o out/alloca.pch
+    $CC include/prprf.h -o out/prprf.pch
+    $CC include/prglobal.h -o out/prglobal.pch
+    $CC include/prlog.h -o out/prlog.pch
+    $CC include/prmem.h -o out/prmem.pch
+    $CC include/prmjtime.h -o out/prmjtime.pch
+    $CC include/prtime.h -o out/prtime.pch
+    $CC include/prlong.h -o out/prlong.pch
+    $CC include/prosdep.h -o out/prosdep.pch
+    $CC include/prunixos.h -o out/prunixos.pch
+    $CC include/prsync.h -o out/prsync.pch
 }
 
-function test_objs() {
+function compile_objs() {
     echo "compiling OBJS..."
-    CC -Iinclude src/mo_array.c -c -o out/mo_array.o
-    CC -Iinclude src/mo_atom.c -c -o out/mo_atom.o
-    CC -Iinclude src/mo_bcode.c -c -o out/mo_bcode.o
-    CC -Iinclude src/mo_bool.c -c -o out/mo_bool.o
-    CC -Iinclude src/mo_cntxt.c -c -o out/mo_cntxt.o
-    CC -Iinclude src/mo_date.c -Wno-dangling-else -c -o out/mo_date.o
-    CC -Iinclude src/mo_emit.c -c -o out/mo_emit.o
-    CC -Iinclude src/mo_fun.c -c -o out/mo_fun.o
-    CC -Iinclude src/mo_math.c -c -o out/mo_math.o
-    CC -Iinclude src/mo_num.c -Wno-non-literal-null-conversion -c -o out/mo_num.o
-    CC -Iinclude src/mo_obj.c -c -o out/mo_obj.o
-    CC -Iinclude src/mo_parse.c -c -o out/mo_parse.o
-    CC -Iinclude src/mo_scan.c -c -o out/mo_scan.o
-    CC -Iinclude src/mo_scope.c -c -o out/mo_scope.o
-    CC -Iinclude src/mo_str.c -Wno-non-literal-null-conversion -c -o out/mo_str.o
-    CC -Iinclude src/mocha.c -c -o out/mocha.o
-    CC -Iinclude src/mochaapi.c -Wno-non-literal-null-conversion -c -o out/mochaapi.o
-    CC -Iinclude src/mochalib.c -c -o out/mochalib.o
-    CC -Iinclude src/prmjtime.c -c -o out/prmjtime.o
-    CC -Iinclude src/prtime.c -c -o out/prtime.o
-    CC -Iinclude src/prarena.c -c -o out/prarena.o
-    CC -Iinclude src/prhash.c -c -o out/prhash.o
-    CC -Iinclude src/prprf.c -c -o out/prprf.o
-    CC -Iinclude src/prdtoa.c \
+    $CC -Iinclude src/mo_array.c -c -o out/mo_array.o
+    $CC -Iinclude src/mo_atom.c -c -o out/mo_atom.o
+    $CC -Iinclude src/mo_bcode.c -c -o out/mo_bcode.o
+    $CC -Iinclude src/mo_bool.c -c -o out/mo_bool.o
+    $CC -Iinclude src/mo_cntxt.c -c -o out/mo_cntxt.o
+    $CC -Iinclude src/mo_date.c -Wno-dangling-else -c -o out/mo_date.o
+    $CC -Iinclude src/mo_emit.c -c -o out/mo_emit.o
+    $CC -Iinclude src/mo_fun.c -c -o out/mo_fun.o
+    $CC -Iinclude src/mo_math.c -c -o out/mo_math.o
+    $CC -Iinclude src/mo_num.c -Wno-non-literal-null-conversion -c -o out/mo_num.o
+    $CC -Iinclude src/mo_obj.c -c -o out/mo_obj.o
+    $CC -Iinclude src/mo_parse.c -c -o out/mo_parse.o
+    $CC -Iinclude src/mo_scan.c -c -o out/mo_scan.o
+    $CC -Iinclude src/mo_scope.c -c -o out/mo_scope.o
+    $CC -Iinclude src/mo_str.c -Wno-non-literal-null-conversion -c -o out/mo_str.o
+    $CC -Iinclude src/mocha.c -c -o out/mocha.o
+    $CC -Iinclude src/mochaapi.c -Wno-non-literal-null-conversion -c -o out/mochaapi.o
+    $CC -Iinclude src/mochalib.c -c -o out/mochalib.o
+    $CC -Iinclude src/prmjtime.c -c -o out/prmjtime.o
+    $CC -Iinclude src/prtime.c -c -o out/prtime.o
+    $CC -Iinclude src/prarena.c -c -o out/prarena.o
+    $CC -Iinclude src/prhash.c -c -o out/prhash.o
+    $CC -Iinclude src/prprf.c -c -o out/prprf.o
+    $CC -Iinclude src/prdtoa.c \
         -Wno-logical-not-parentheses \
         -Wno-shift-op-parentheses \
         -Wno-parentheses \
         -c -o out/prdtoa.o
-    CC -Iinclude src/log2.c -c -o out/log2.o
-    CC -Iinclude src/longlong.c -c -o out/longlong.o
+    $CC -Iinclude src/log2.c -c -o out/log2.o
+    $CC -Iinclude src/longlong.c -c -o out/longlong.o
 }
 
-function test_main() {
+function compile_native() {
+    export CC=clang
+    export AR=ar
+    compile_objs
     echo "linking..."
-    AR -rcs out/libmocha.a out/*.o
-    CC -Iinclude -Lout -lmocha tests/mo_shell.c -o out/mo_shell
+    $AR -rcs out/libmocha.a out/*.o
+    $CC -Iinclude -Lout -lmocha tests/mo_shell.c -o out/mo_shell
     echo "mocha shell compiled!"
 }
 
+function compile_web() {
+    export CC=emcc
+    export AR=emar
+    compile_objs
+    echo "linking..."
+    $AR -rcs out/libmocha.a out/*.o
+    $CC -Iinclude -Lout -lmocha tests/mo_shell.c \
+        --shell-file src/shell.html \
+        -s WASM=$1 \
+        -o $2
+    echo "mocha shell compiled!"
+}
+
+function compile_js() {
+    compile_web 0 out/mocha_shell_js.html
+}
+
+function compile_wasm() {
+    compile_web 1 out/mocha_shell_wasm.html
+}
+
 mkdir -p out
-# test_headers
-test_objs
-test_main
